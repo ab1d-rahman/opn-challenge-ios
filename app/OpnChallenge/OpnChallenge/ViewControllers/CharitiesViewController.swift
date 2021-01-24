2ff0d8db9995474af224eb39a473c357009de70b
Abid Rahman <abid.rahman@dsinnovators.com>
2021-01-24 22:12:21 +0600

    Show loaded charities data from API using TableView

--- app/OpnChallenge/OpnChallenge/ViewControllers/CharitiesViewController.swift
+++ app/OpnChallenge/OpnChallenge/ViewControllers/CharitiesViewController.swift
@@ -8,11 +8,47 @@
 import UIKit
 
 class CharitiesViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
     private let viewModel = CharitiesViewModel(charitiesService: CharitiesService(httpClient: HTTPClient()))
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.setupTableView()
+
+        self.viewModel.delegate = self
         self.viewModel.fetchCharities()
+    }
+
+    private func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+
+        self.tableView.tableFooterView = UIView()
+    }
+}
+
+extension CharitiesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.viewModel.numberOfCharities()
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: CharitiesTableViewCell.reuseIdentifier, for: indexPath) as? CharitiesTableViewCell else {
+            return UITableViewCell()
+        }
+
+        cell.customize(using: self.viewModel.getCellViewModel(at: indexPath.row))
+
+        return cell
+    }
+}
+
+extension CharitiesViewController: CharitiesViewModelDelegate {
+    func didFetchCharities() {
+        DispatchQueue.main.async { [weak self] in
+            self?.tableView.reloadData()
+        }
     }
 }
