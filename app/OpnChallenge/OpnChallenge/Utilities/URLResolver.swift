e4d3d285f94ce579fb8a62dc5ac4fda698d8c8aa
Abid Rahman <ab1d.rahman3991@gmail.com>
2021-01-28 03:50:37 +0600

    Update baseURL

--- app/OpnChallenge/OpnChallenge/Utilities/URLResolver.swift
+++ app/OpnChallenge/OpnChallenge/Utilities/URLResolver.swift
@@ -10,7 +10,7 @@ import Foundation
 public class URLResolver {
     public static let shared = URLResolver()
 
-    private let baseURL = "http://localhost:8080/"
+    private let baseURL = "http://192.168.1.14:8080/"
 
     private init() {}
 
